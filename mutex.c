46c5dfe4731b63807f90b83e321404a0f381c4b2
chirag627 <56315673+chirag627@users.noreply.github.com>
2020-04-08 02:00:10 +0530

    Update mutex.c

--- mutex.c
+++ mutex.c
@@ -1,4 +1,7 @@
-/*Write a program to create two threads and both the threads are sharing a common variable whose value is initialized by 2. First thread should increment the shared value by 1 and the other thread should decrement by 1. In order to avoid context switching and to attain consistency use Mutex Lock. Create a scenario such that after every execution, the value of the shared variable should be consistent. */
+/*Write a program to create two threads and both the threads are sharing a common variable whose value is initialized by 2. 
+First thread should increment the shared value by 1 and the other thread should decrement by 1. In order to avoid context
+switching and to attain consistency use Mutex Lock. Create a scenario such that after every execution, the value of the 
+shared variable should be consistent. */
 
 #include<pthread.h>
 #include<stdio.h>
